767bfdaf3ad4122020909a3ed77c6d5be9bcb48e
Monde Nel <Mondenel1996@gmail.com>
2025-04-14 11:48:49 +0200

    Added migrations and create a sql database with tables

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -4,8 +4,4 @@ from . import views
 urlpatterns = [
     # URL pattern for listing and creating blog posts
     path('blogposts/', views.BlogPostListCreate.as_view(), name='blogpost-list-create'),
-
-    # URL pattern for retrieving, updating, and deleting a specific blog post by its ID
-    # pk = Primary Key, which is the ID of the blog post
-    path('blogposts/<int:pk>/', views.BlogPostDetail.as_view(), name='blogpost-detail'),
 ]
