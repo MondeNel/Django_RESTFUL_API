a6db45bf282491c44c372199cb6bfa4e2398b010
Monde Nel <Mondenel1996@gmail.com>
2025-04-14 11:26:05 +0200

    Create BlogPostListCreate view for retrieving and creating blog posts

--- mysite/api/serializers.py
+++ mysite/api/serializers.py
@@ -9,18 +9,3 @@ class BlogPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogPost
         fields = ['id', 'title', 'content', 'created_at', 'updated_at']
-
-    def create(self, validated_data):
-        """
-        Create and return a new BlogPost instance, given the validated data.
-        """
-        return BlogPost.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing BlogPost instance, given the validated data.
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.content = validated_data.get('content', instance.content)
-        instance.save()
-        return instance
