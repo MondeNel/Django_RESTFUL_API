c01c2b2007fe5a2e870dff661f3cb1a8364bfebf
Monde Nel <Mondenel1996@gmail.com>
2025-04-14 11:26:50 +0200

    Create BlogPostListCreate view for retrieving and creating blog posts

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import BlogPost
+from .serializers import BlogPostSerializer
 
 # Create your views here.
+
+class BlogPostListCreate(generics.ListCreateAPIView):
+    """
+    API view to retrieve and create blog posts.
+
+    GET: Returns a list of all blog posts.
+    POST: Creates a new blog post from the request data and returns the created post.
+    """
+    # Define the queryset that will be used to retrieve blog posts.
+    queryset = BlogPost.objects.all()
+
+    # Define the serializer class that will be used to convert data to and from JSON format.
+    serializer_class = BlogPostSerializer
